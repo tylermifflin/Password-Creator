03262af01279a8fdc9f557c4e8481eab8d798f93
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-03-07 16:41:45 -0700

    I set up prompt to ask if they want numbers

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ function generatePassword() {
   console.log(response);
   var response = prompt("Do you want to include uppercase letters?");
   console.log(response);
+  var response = prompt("Do you want to include numbers?");
+  console.log(response);
 }
   // check if password length is between 8 and 128 characters
   // if (passwordLength < 8 || passwordLength > 128) {
