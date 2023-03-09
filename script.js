57ad9bc51a7fa72d4e61b04d1f6236524318c998
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-03-09 15:15:17 -0700

    I changed the variable response to length, includelowercase, includeuppercase, includenumbers,includespecialcharacters so that I can create some if statements, I also moved my arrays into the function value so they would be defined

--- script.js
+++ script.js
@@ -1,29 +1,27 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var passwordLength = > 8 && < 128;
-var lowercase = "abcdefghijklmnopqrstuvwxyz";
-var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numbers = "0123456789";
-var specialCharacters = "!@#$%^&*()_+~`|}{[]\:;?><,./-=";
+
 
 function generatePassword() {
   // promp user for password length
-  var response = prompt("How many characters would you like your password to be?");
-  console.log(response);
-  var response = confirm("Do you want to include lowercase letters?");
-  console.log(response);
-  var response = confirm("Do you want to include uppercase letters?");
-  console.log(response);
-  var response = confirm("Do you want to include numbers?");
-  console.log(response);
-  var response = confirm("Do you want to include special characters?");
-  console.log(response);
+  var length = prompt("How many characters would you like your password to be? Length must be between 8 and 128 characters");
+  console.log(length);
+  
+  var includelowercase = confirm("Do you want to include lowercase letters?");
+  console.log(includelowercase);
+  var includeuppercase = confirm("Do you want to include uppercase letters?");
+  console.log(includeuppercase);
+  var includenumbers = confirm("Do you want to include numbers?");
+  console.log(includenumbers);
+  var includespecialcharacters = confirm("Do you want to include special characters?");
+  console.log(includespecialcharacters);
+
+  var lowercase = "abcdefghijklmnopqrstuvwxyz";
+  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numbers = "0123456789";
+  var specialcharacters = "!@#$%^&*()_+~`|}{[]\:;?><,./-=";
+
 }
-  //check if password length is between 8 and 128 characters
-  //if (passwordLength < 8 || passwordLength > 128) {
-   //alert("Password length must be between 8 and 128 characters");
-  
-  
 
 // Write password to the #password input
 function writePassword() {
