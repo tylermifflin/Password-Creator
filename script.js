0b59dc19911d8d6d289a2dcb1e6576958582c12f
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-03-09 14:29:20 -0700

    I changed the var response from prompt to confirm for the yes no question to make it more simple

--- script.js
+++ script.js
@@ -5,18 +5,19 @@ function generatePassword() {
   // promp user for password length
   var response = prompt("How many characters would you like your password to be?");
   console.log(response);
-  var response = prompt("Do you want to include lowercase letters?");
+  var response = confirm("Do you want to include lowercase letters?");
   console.log(response);
-  var response = prompt("Do you want to include uppercase letters?");
+  var response = confirm("Do you want to include uppercase letters?");
   console.log(response);
-  var response = prompt("Do you want to include numbers?");
+  var response = confirm("Do you want to include numbers?");
   console.log(response);
-  var response = prompt("Do you want to include special characters?");
+  var response = confirm("Do you want to include special characters?");
   console.log(response);
 }
-  // check if password length is between 8 and 128 characters
-  // if (passwordLength < 8 || passwordLength > 128) {
-  //  alert("Password length must be between 8 and 128 characters");
+  //check if password length is between 8 and 128 characters
+  //if (passwordLength < 8 || passwordLength > 128) {
+   //alert("Password length must be between 8 and 128 characters");
+  
   
 
 // Write password to the #password input
